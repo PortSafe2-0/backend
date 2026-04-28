437ac1493de0e3d3fc178f5f0540e5bfc25eb1fe
Maria Eduarda Claro <eduardaclaro@MacBook-Air-de-Maria.local>
2026-04-28 10:52:42 -0300

    :wrench: chore: register user repository in DI container in Program.cs

--- portsafe_api/Program.cs
+++ portsafe_api/Program.cs
@@ -1,17 +1,17 @@
 using PortSafe.API.Data;
 using Microsoft.EntityFrameworkCore;
-
+using PortSafe.API.Interfaces;
+using PortSafe.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
